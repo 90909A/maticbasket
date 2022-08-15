413adf2b5a8120c927884146f64ea287aef4417b
Mda <ktrnstm@gmail.com>
2022-08-15 13:32:16 +0300

    media start

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 new ClipboardJS('#copy');
+
+const input = document.querySelector('#copy__input');
+const inputFake = document.querySelector('#copy__input-fake');
+const copyBtn = document.querySelector('#copy');
+
+const inputPrevValue = input.value;
+
+copyBtn.addEventListener('click', () => {
+    inputFake.value = '✓';
+    setTimeout(() => inputFake.value = inputPrevValue, 800)
+})
+
+input.addEventListener('click', () => {
+    this.select();
+})
